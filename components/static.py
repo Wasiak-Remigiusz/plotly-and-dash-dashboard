dd8bdf7ed5c2f45fe30a3f4cfd4554bf504c5824
Remigiusz <wasiak.remigiusz@gmail.com>
2025-03-24 16:03:12 +0100

    Navbar updated

--- components/static.py
+++ components/static.py
@@ -10,7 +10,11 @@ navbar = dbc.Navbar(
             dbc.Row(
                 [
                     dbc.Col(html.Img(src=world_logo, height="90px")),
-                    dbc.Col(dbc.NavbarBrand("World data Dashboard", className="ms-3 nav-title")),
+                    dbc.Col(dbc.NavbarBrand([
+        "World data ",
+        html.Span("Dash", style={"color": "#3f4f75"}), # Dash Color
+        "board", html.Span(".", style={"color": "rgba(127,205,189,1)", "fontSize": "3.5rem"})  # Dash Color
+    ], className="ms-3 nav-title")),
                 ],
                 align="center",
                 className="g-0",
